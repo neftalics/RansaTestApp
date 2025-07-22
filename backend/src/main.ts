376201c48fb234884e10e51b134909f7bab79a52
neftali <54753789+neftalics@users.noreply.github.com>
2025-07-22 13:15:53 -0500

    Update main.ts apaño

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://ransatestapp-1.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
